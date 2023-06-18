1d67af8ae01ce72dd59803fb701aa52c3a74feef
renatapigatto <renatapigatto@gmail.com>
2023-06-18 15:49:51 -0300

    add header, emnu, and menu button components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css"
 
-function Header(){
+export default function Header(){
     return(
         <header>
             <div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import './Home.css'
 import Card from "../Components/Card/Card";
+import Header from "../Components/Header/Header"
 
 export default function Home() {
     return (
         <>
-        {/* <Header/> */}
+        {/* <Header /> */}
         <div>
-            <h1>GUIA DO ESTUDANTE</h1>
+            <h1>GUIA DO ALUNO</h1>
         </div>
         <div className="menu-grid">
             <Card />
