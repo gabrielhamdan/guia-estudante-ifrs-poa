9fcca1765920de13d25709c96aee7db2848f2bfa
Gabriel Hamdan <gabriel_hamdan@hotmail.com>
2023-06-25 14:23:34 -0300

    feat: servidor replit para trazer dados json

--- src/Services/consumer.js
+++ src/Services/consumer.js
@@ -1,10 +1,11 @@
 async function consomeAPI(pagina) {
-    const response = await fetch(`http://localhost:3000/${pagina}`)
+    const response = await fetch("https://ifrs-json-server.gabrielhamdan.repl.co")
+    // const response = await fetch(`http://localhost:3000/${pagina}`)
 
     const data = await response.json()
 
-    if(response.ok) {
-        return await data
+    if(response.ok && data[pagina]) {
+        return await data[pagina]
     }
 
     return {}
